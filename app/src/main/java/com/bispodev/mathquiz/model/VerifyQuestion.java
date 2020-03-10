81f891735f64ed50b0af9283a7d5626a7d7f5a29
João Gustavo <57817224+bispodev@users.noreply.github.com>
2020-03-09 22:22:31 -0300

    Verify Question
    Verify question

--- app/src/main/java/com/bispodev/mathquiz/model/VerifyQuestion.java
+++ app/src/main/java/com/bispodev/mathquiz/model/VerifyQuestion.java
@@ -1,4 +1,8 @@
 package com.bispodev.mathquiz.model;
 
 public class VerifyQuestion {
+
+    public boolean isRCorreta(Question question, double resposta){
+        return question.getrCorreta() == resposta;
+    }
 }
